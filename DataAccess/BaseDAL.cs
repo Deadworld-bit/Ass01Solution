9bc10ce174a810c15b6e43eb73cd1939d1c52f33
Deadworld-bit <67327661+Deadworld-bit@users.noreply.github.com>
2022-07-04 13:28:21 +0700

    Add project files.

---
+++ DataAccess/BaseDAL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccess
+{
+    public class BaseDAL
+    {
+        public AssignmentDataProvider dataProvider { get; set; } = null;
+        public SqlConnection connection = null;
+
+        public BaseDAL()
+        {
+            var connectionString = GetConnectionString();
+            dataProvider = new AssignmentDataProvider(connectionString);
+        }
+
+        public string GetConnectionString()
+        {
+            string connectionString;
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetting.json", true, true).Build();
+            connectionString = config["ConnectionString:MyStockDB"];
+            return connectionString;
+        }
+
+        public void CloseConneciton() => dataProvider.CloseConnection(connection);
+    }
+}
+
---
+++ MyStoreWinApp/frmMemberDetail.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyStoreWinApp
+{
+    public partial class frmMemberDetail : Form
+    {
+        public frmMemberDetail()
+        {
+            InitializeComponent();
+        }
+        public IMemberRepository MemberRepository { get; set; }
+        public 
+        private void frmMemberDetail_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
